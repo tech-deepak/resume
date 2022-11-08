16f57cff64658f3d0b1e7c5885606ef2ab650e07
Deepak Kumar <deepak@Deepaks-MacBook-Pro.local>
2022-11-08 23:56:09 +0530

    updated file changes

--- assets/vendor/main.js
+++ assets/vendor/main.js
@@ -380,21 +380,6 @@ loadTypeTextAnimation()
 		return false;
 	});
 
-	// jQuery counterUp
-	$('[data-toggle="counter-up"]').counterUp({
-		delay: 10,
-		time: 1000
-	});
-
-	// Skills section
-	$('.skills-content').waypoint(function () {
-		$('.progress .progress-bar').each(function () {
-			$(this).css("width", $(this).attr("aria-valuenow") + '%');
-		});
-	}, {
-		offset: '80%'
-	});
-
 	// Init AOS
 	function aos_init() {
 		AOS.init({
@@ -427,22 +412,6 @@ loadTypeTextAnimation()
 		// Initiate aos_init() function
 		aos_init();
 
-	});
-
-	// Testimonials carousel (uses the Owl Carousel library)
-	$(".testimonials-carousel").owlCarousel({
-		autoplay: true,
-		dots: true,
-		loop: true,
-		items: 1
-	});
-
-	// Portfolio details carousel
-	$(".portfolio-details-carousel").owlCarousel({
-		autoplay: true,
-		dots: true,
-		loop: true,
-		items: 1
 	});
 
 	setTimeout(() => {
